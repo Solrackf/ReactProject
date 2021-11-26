11aa2012fd024c0b6da9aa950d610b03ae41c853
Solrackf <carlosescorcia81@gmail.com>
2021-11-26 04:11:30 -0500

    Delete all files that not be used

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from './routes/post.js'
+
+const app = express();
+
+app.use('/post',postRoutes)
+
+app.use(bodyParser.json({limit: "30mb", extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://XicBeC0SjCRoELMT:XicBeC0SjCRoELMT123@cluster0.uohou.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000
+console.log(PORT);
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
+    .then(()=>app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error)=>console.log(error.message));
+mongoose.connect(CONNECTION_URL).then(()=>{console.log('...')});
