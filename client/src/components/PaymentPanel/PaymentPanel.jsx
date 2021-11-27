5ed27c40da7593df8363e74e60aa67299589e239
AndersonDavi <andesondavid17@gmail.com>
2021-11-26 23:35:40 -0500

    inicio rama, componente de pago

---
+++ client/src/components/PaymentPanel/PaymentPanel.jsx
@@ -0,0 +1,46 @@
+import './PaymentPanel.css'
+// import WaveOne from '../../WaveOne.svg'
+// import WaveTwo from '../../WavesTwo.svg'    
+
+export default function PaymentPanel(props){
+    return (
+        <div className="paycontainer">
+            <div className="info">
+            <ul>
+                <li>
+                    <h2>Referencia De Pago</h2>
+                    <p>3216549870</p>
+                </li>
+                <li>
+                    <h2>Valor A Pagar</h2>
+                   <p>$73.000</p>
+                </li>
+                <li>
+                    <h2>Fecha Oportuna De Pago</h2>
+                    <p>nov/24/2021</p>
+                </li>
+                <li>
+                    <h2>Fecha De Pago</h2>
+                    <p>nov/18/2021</p>
+                </li>
+            </ul>
+        </div>
+        <div className="factura">
+            <ul>
+                <li>
+                    <h2>Correo Electrónico</h2>
+                    <p>usuario@gmail.com</p>
+                </li>
+                <li>
+                    <h2>Métodos De Pago</h2>
+                    <p>EFECTY</p>
+                    <p>PSE</p>
+                </li>
+                <li>
+                    <button class="FormBtn">Pagar</button>
+                </li>
+            </ul>
+        </div>
+        </div>
+    )
+}
