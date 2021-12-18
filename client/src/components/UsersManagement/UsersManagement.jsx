4fae5f1e5b8780218b971cd408c8e2e52428cccc
SaraLucia8 <slpuerta@uninorte.edu.co>
2021-12-18 15:22:31 -0500

    Read users

--- client/src/components/UsersManagement/UsersManagement.jsx
+++ client/src/components/UsersManagement/UsersManagement.jsx
@@ -1,22 +1,32 @@
+import { useSelector } from 'react-redux';
+
 import './UsersManagement.css'
 import Edit from "../../edit.png"
-import Visible from "../../visible.png"
+// import Visible from "../../visible.png"
 import Delete from "../../delete.png"
+import { updatedUser } from '../../actions/users';
+import { useDispatch } from 'react-redux';
 
-export default function UsersManagement(props){
+
+export default function UsersManagement({ user }){
+    const users = useSelector((state) => state.users);
     return (
         <div className="user-management">
+            {users.map((user) => (
             <div className="container-user-management">
                 <div className="info-user-management">
-                    <span className="title-user-management">John Doe</span>
-                    <span className="subtitle-user-management">ID: 1234567890</span>
+                    <span className="title-user-management">{user.name}</span>
+                    <span className="subtitle-user-management">ID: {user.identification}</span>
+                    <span className="subtitle-user-management">Teléfono: {user.phone}</span>
+                    <span className="subtitle-user-management">Email: {user.email}</span>
                 </div>
                 <div className="icons-user-management">
                     <button className="btn-user-management"><img className="icon-user-management" src={Edit} alt="" id="Edit"/></button>
-                    <button className="btn-user-management"><img className="icon-user-management" src={Visible} alt="" id="Visible"/></button>
+                    {/* <button className="btn-user-management"><img className="icon-user-management" src={Visible} alt="" id="Visible"/></button> */}
                     <button className="btn-user-management"><img className="icon-user-management" src={Delete} alt="" id="Delete"/></button>
                 </div>
             </div>
+            ))}
         </div>
     )
 }
